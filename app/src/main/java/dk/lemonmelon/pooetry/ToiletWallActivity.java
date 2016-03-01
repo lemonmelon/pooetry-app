e2c10bd0bddc5ba50c3576d8a8a8b2532cbf0f7d
Niels Abildgaard <niels.abildgaard@gmail.com>
2016-03-01 12:47:09 +0100

    Cleaned up. Added a x+y scrolling view. Loading data from API

--- app/src/main/java/dk/lemonmelon/pooetry/ToiletWallActivity.java
+++ app/src/main/java/dk/lemonmelon/pooetry/ToiletWallActivity.java
@@ -1,10 +1,28 @@
 package dk.lemonmelon.pooetry;
 
 import android.app.Activity;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.LinearLayout;
+import android.util.Log;
+import android.widget.HorizontalScrollView;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 
 public class ToiletWallActivity extends Activity {
@@ -14,30 +32,100 @@ public class ToiletWallActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toilet_wall);
 
-        LinearLayout container = (LinearLayout) findViewById(R.id.content_container);
+        RelativeLayout container = (RelativeLayout) findViewById(R.id.content_container);
 
-        
+        ScrollView verticalScrollView = (ScrollView) findViewById(R.id.vertical_scroll_view);
+        HorizontalScrollView horizontalScrollView = (HorizontalScrollView) findViewById(R.id.horizontal_scroll_view);
+
+        //TODO: Make this scrolly shit work so we start in center
+        //verticalScrollView.scrollTo(0, 15);
+        //horizontalScrollView.scrollTo(5000, 0);
+        //Log.d("pooetry", "scrolled");
+
+        startContentRequestTask(this, container);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_toilet_wall, menu);
-        return true;
+    private void startContentRequestTask(Context ctx, RelativeLayout container) {
+        new ContentRequestTask(ctx, container).execute();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private class ContentRequestTask extends AsyncTask<Void, Void, Void> {
+        private RelativeLayout container;
+        private Context ctx;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        public ContentRequestTask(Context ctx, RelativeLayout container) {
+            this.ctx = ctx;
+            this.container = container;
         }
 
-        return super.onOptionsItemSelected(item);
+        protected Void doInBackground(Void... nothings) {
+            final RelativeLayout container = this.container;
+            final Context ctx = this.ctx;
+
+            InputStream bodyStream = null;
+            try {
+                HttpClient httpClient = new DefaultHttpClient();
+                HttpResponse res = httpClient.execute(new HttpGet("http://api.pooetry.lemonmelon.dk:3009/content?long=x&lat=y"));
+                if(res.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
+                    Log.e("pooetry", "Failed to get content. Instead, got " + res);
+                    return null;
+                }
+                bodyStream = res.getEntity().getContent();
+            }
+            catch(IOException e) {
+                Log.e("pooetry", "Got IOException while getting data from online " + e);
+                return null;
+            }
+
+            BufferedReader r = new BufferedReader(new InputStreamReader(bodyStream));
+            StringBuilder bodyStr = new StringBuilder();
+            String line;
+            try {
+                while ((line = r.readLine()) != null) {
+                    bodyStr.append(line);
+                }
+            }
+            catch(IOException e) {
+                Log.e("pooetry", "Got IOException while reading body stream " + e);
+                return null;
+            }
+
+            try {
+                JSONObject body = new JSONObject(bodyStr.toString());
+                JSONArray notes = (JSONArray) body.get("notes");
+
+                if(notes.length() < 1) {
+                    Log.d("pooetry", "No notes");
+                    return null;
+                }
+
+                final String[] result = new String[notes.length()];
+
+                for(int i = 0; i < notes.length(); i++) {
+                    String note = notes.getString(i);
+                    result[i] = note;
+                }
+
+                Log.d("pooetry", "Got resulting strings: " + result);
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        for(int i = 0; i < result.length; i++) {
+                            TextView v = new TextView(ctx);
+                            v.setText(result[i]);
+                            container.addView(v);
+                        }
+                    }
+                });
+
+                return null;
+            }
+            catch(JSONException e) {
+                Log.e("pooetry", "Got JSONException while parsing body string " + e);
+            }
+
+            return null;
+        }
     }
 }
