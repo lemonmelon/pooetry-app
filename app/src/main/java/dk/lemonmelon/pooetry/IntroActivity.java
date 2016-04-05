90cc03994e8dedd65030e9da3342d0a344e0db28
Niels Abildgaard <niels.abildgaard@gmail.com>
2016-04-05 22:15:59 +0200

    Trigger location getting on introactivity if none exists already

--- app/src/main/java/dk/lemonmelon/pooetry/IntroActivity.java
+++ app/src/main/java/dk/lemonmelon/pooetry/IntroActivity.java
@@ -1,13 +1,21 @@
 package dk.lemonmelon.pooetry;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Criteria;
+import android.location.GpsStatus;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class IntroActivity extends Activity {
+    private Location _location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +38,36 @@ public class IntroActivity extends Activity {
                 startActivity(showToiletWall);
             }
         });
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        _location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        if(_location != null) {
+            return;
+        }
+
+        triggerLoadingOfLocation(locationManager);
+    }
+
+    public void triggerLoadingOfLocation(LocationManager locationManager) {
+        final Context ctx = this;
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+
+        locationManager.requestSingleUpdate(criteria, new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                _location = location;
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            @Override
+            public void onProviderEnabled(String provider) {}
+
+            @Override
+            public void onProviderDisabled(String provider) {}
+        }, null);
     }
 }
